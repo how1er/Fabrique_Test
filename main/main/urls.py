94234296e41e49958c5d24c5180818813dfe5d9e
how1er <azamatkfu@gmail.com>
2022-12-08 18:35:08 +0300

    add urls

--- main/main/urls.py
+++ main/main/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from notification_service.views import ClientViewSet, MailingViewSet, MessageViewSet
+
+router = routers.DefaultRouter()
+router.register(r'clients', ClientViewSet)
+router.register(r'messages', MessageViewSet)
+router.register(r'mailings', MailingViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
